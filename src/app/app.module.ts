88eb747ed375bf1f9de032f6fce64eb4fcb61163
brendaschussler <brendaschussler@gmail.com>
2023-03-18 21:55:25 -0300

    storeheader

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { StoreComponent } from './components/store/store.component';
+import { LibraryComponent } from './components/library/library.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    StoreComponent,
+    LibraryComponent
   ],
   imports: [
     BrowserModule,
