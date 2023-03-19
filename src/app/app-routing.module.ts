506493e124af3c43c3c155ff1f5ccf076a39c82a
brendaschussler <brendaschussler@gmail.com>
2023-03-18 23:07:07 -0300

    library

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { StoreComponent } from './components/store/store.component';
+import { LibraryComponent } from './components/library/library.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'store', component: StoreComponent },
+  { path: 'library', component: LibraryComponent },
+  { path: '', redirectTo: '/store', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
